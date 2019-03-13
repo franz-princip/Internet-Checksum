3a21ffe4c851ee4147359520fd68b2d34781f449
Walter S <wsellars123@gmail.com>
2019-03-13 18:18:20 -0400

    Create checksum_verify.py

---
+++ checksum_verify.py
@@ -0,0 +1,13 @@
+hexinput = '0x'+raw_input('Input hex checksum to verify:')
+hexinput = "".join(hexinput.split())
+quads = [int(hexinput[i:i+4],16) for i in range(2, len(hexinput), 4)]
+sumstuff = sum(quads)
+if len(hex(sumstuff)) > 6:
+  manip = str(hex(sumstuff))
+  sumstuff = int(manip[2],16)+int(manip[3:],16)
+
+print("Sum of 16bit groups is: "+ (hex(sumstuff))[2:])
+if str((hex(sumstuff))[2:])
+  print("No Error")
+else:
+  print("Error")
